e4590330c97be243d69748cc74f06590cefe61e2
AleixTP23219 <ateresp17@alumnes.ilerna.com>
2026-04-13 17:04:46 +0200

    Interface e implementacio
    2 funcions

--- RA6-PT1-TEMPERATURE-CALCULATOR/src/main/java/es/ilerna/M0486/ra6/pt1/calculator/api/TemperatureCalculator.java
+++ RA6-PT1-TEMPERATURE-CALCULATOR/src/main/java/es/ilerna/M0486/ra6/pt1/calculator/api/TemperatureCalculator.java
@@ -4,5 +4,9 @@ import java.util.List;
 
 public interface TemperatureCalculator {
 
-	// TODO
+    double average(List<Double> temperatures);
+    double max(List<Double> temperatures);
+    double min(List<Double> temperatures);
+    double celsiusToFahrenheit(double celsius);
+    double celsiusToKelvin(double celsius);
 }
--- RA6-PT1-TEMPERATURE-CALCULATOR/src/main/java/es/ilerna/M0486/ra6/pt1/calculator/impl/TemperatureCalculatorImpl.java
+++ RA6-PT1-TEMPERATURE-CALCULATOR/src/main/java/es/ilerna/M0486/ra6/pt1/calculator/impl/TemperatureCalculatorImpl.java
@@ -5,6 +5,15 @@ import java.util.List;
 import es.ilerna.M0486.ra6.pt1.calculator.api.TemperatureCalculator;
 
 public class TemperatureCalculatorImpl implements TemperatureCalculator {
-	
-	// TODO Implement methods
+	@Override
+    public double celsiusToKelvin(double celsous) {
+        celsous = celsous + 273.15;
+        return celsous;
+    }
+
+    public double celsiusToFahrenheit (double celsous){
+        celsous = (celsous*1.8)+32; //es lo mateix que celsous 1.8=9/5
+        return celsous;
+    }
+
 }
